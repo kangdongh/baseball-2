3a8eb23527b83483c6f7a3106fa0cc15b6c0b6b5
jeff.kang <jeff.kang@samsung.com>
2024-07-10 14:01:02 +0900

    [refactoring] change parameter names

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 class Game:
     _answer: str
 
-    def set_answer(self, param):
-        if len(param) != 3:
+    def set_answer(self, digits: str):
+        if len(digits) != 3:
             raise ValueError()
+        self._answer = digits
 
-    def guess(self, param):
-        if len(param) != 3:
+    def guess(self, digits: str):
+        if len(digits) != 3:
             raise ValueError()
